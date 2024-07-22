2b46925a44afcb4fd603688ad6039232512d547e
DeadRain Code <kepera@inbox.ru>
2024-07-22 15:45:31 +0300

    app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import { useEffect } from 'react';
 import './App.css';
 const tg = window.Telegram.WebApp;
 
+const theme = {
+  bg_color: '#000',
+  text_color: '#ffffff',
+  hint_color: '#aaaaaa',
+  link_color: '#8774e1',
+  button_color: '#8774e1',
+  button_text_color: '#ffffff',
+  secondary_bg_color: '#0f0f0f',
+  header_bg_color: '#000',
+};
+
 function App() {
 
   useEffect(() => {
+    
+    if (showModal) {
+      tg.themeParams(theme)
+    }
     tg.ready();
-    tg.setHeaderColor("#000")
-    tg.setBackgroundColor("#000")
-  }, []);
+    
+  }, [showModal]);
 
   const onClose = () => {
     tg.close()
